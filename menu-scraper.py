d444cf43ca5b53713af4c57d4038239a7fc7d6f4
Andy Bautista <odfelip.bautista@gmail.com>
2024-07-29 11:29:38 -0500

    Add comment

--- menu-scraper.py
+++ menu-scraper.py
@@ -1,4 +1,5 @@
 # Goals of this script
 # 1) Get the specials for each day in the current week
 # 2) Send a text message/email at a specific time about the current day's special
-
+# https://www.geeksforgeeks.org/how-to-scrape-all-pdf-files-in-a-website/
+# The website above should help with this
